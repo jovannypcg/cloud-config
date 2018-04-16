f300fedfcf90d3513c3848a44e96b0a940dba9b9
Jovanny Cruz <jovannypcg@yahoo.com>
2018-04-16 12:47:58 -0500

    Enable Config Server

--- src/main/java/com/figureeight/cserver/CloudServerApplication.java
+++ src/main/java/com/figureeight/cserver/CloudServerApplication.java
@@ -2,7 +2,9 @@ package com.figureeight.cserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class CloudServerApplication {
 
